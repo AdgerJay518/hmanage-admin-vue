bac8116b9719c704c578d0ec70a38fe0c494312c
wzj <994057864@qq.com>
2021-10-28 18:18:53 +0800

    feat:
    专题管理页面

---
+++ src/api/homeSubject.js
@@ -0,0 +1,41 @@
+import request from "../axios";
+
+export function fetchList(params) {
+    return request({
+        url:'/subjectCategory/list',
+        method:'get',
+        params:params
+    })
+}
+
+export function updateRecommendStatus(data) {
+    return request({
+        url:'/home/recommendSubject/update/recommendStatus',
+        method:'post',
+        data:data
+    })
+}
+
+export function deleteHomeSubject(data) {
+    return request({
+        url:'/home/recommendSubject/delete',
+        method:'post',
+        data:data
+    })
+}
+
+export function createHomeSubject(data) {
+    return request({
+        url:'/home/recommendSubject/create',
+        method:'post',
+        data:data
+    })
+}
+
+export function updateHomeSubjectSort(params) {
+    return request({
+        url:'/home/recommendSubject/update/sort/'+params.id,
+        method:'post',
+        params:params
+    })
+}
---
+++ src/api/subject.js
@@ -0,0 +1,16 @@
+import request from "../axios";
+
+export function fetchListAll() {
+    return request({
+        url:'/subject/listAll',
+        method:'get',
+    })
+}
+
+export function fetchList(params) {
+    return request({
+        url:'/subject/list',
+        method:'get',
+        params:params
+    })
+}
